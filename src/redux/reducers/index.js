3630755f3109e78da1ff1ce9a0deae841815f916
Perryno <andreacaforio74@gmail.com>
2023-04-06 20:44:42 +0200

    first commit

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+  favourites: {
+    content: []
+  }
+};
+
+const MainReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default MainReducer;
